8e76ec1ce50babc7c0b30ad0b52ac5dde634bfe7
Vadim Gera <horsespb@yandex.ru>
2024-08-18 15:34:28 +0300

    Токен

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,14 +1,14 @@
 from django.urls import path
 
 from .apps import AccountsConfig
-from .views import RegisterView
-# from rest_framework_simplejwt.views import TokenRefreshView
+from .views import RegisterView, MyTokenObtainPairView
+from rest_framework_simplejwt.views import TokenRefreshView
 
 app_name = AccountsConfig.name
 
 urlpatterns = [
-    # path('token/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
-    # path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('token/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     # path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
     path('register/', RegisterView.as_view(), name='register'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from rest_framework.permissions import AllowAny
 
+from rest_framework_simplejwt.views import TokenObtainPairView
 from rest_framework import generics
 from .serializers import RegisterSerializer
 
@@ -8,3 +9,7 @@ from .serializers import RegisterSerializer
 class RegisterView(generics.CreateAPIView):
     permission_classes = (AllowAny,)
     serializer_class = RegisterSerializer
+
+
+class MyTokenObtainPairView(TokenObtainPairView):
+    permission_classes = (AllowAny,)
